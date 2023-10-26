5fbe7f2c3ee6a4d6f38bbaeb463a81d6e21c73a5
Jak Crow <jak@jxcrw.io>
2023-10-26 01:40:00 -0700

    +fltr.1: Clean up UI

--- flutter/wordpair/lib/main.dart
+++ flutter/wordpair/lib/main.dart
@@ -2,9 +2,11 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+
 void main() {
   runApp(MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -17,13 +19,14 @@ class MyApp extends StatelessWidget {
         title: 'tagg',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreen),
         ),
         home: MyHomePage(),
       ),
     );
   }
 }
+
 
 class MyAppState extends ChangeNotifier {
   var wordpair = WordPair.random();
@@ -34,6 +37,7 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
+
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -41,18 +45,50 @@ class MyHomePage extends StatelessWidget {
     var wordpair = appState.wordpair;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A random wordpair:'),
-          Text(wordpair.asLowerCase),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BigCard(wordpair: wordpair),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                appState.getNext();
+              },
+              child: Icon(Icons.refresh),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
 
-          ElevatedButton(
-            onPressed: () {
-              appState.getNext();
-            },
-            child: Text('Next'),
-          )
-        ],
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.wordpair,
+  });
+
+  final WordPair wordpair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(
+          wordpair.asLowerCase,
+          style: style,
+          semanticsLabel: "${wordpair.first} ${wordpair.second}",
+        ),
       ),
     );
   }
